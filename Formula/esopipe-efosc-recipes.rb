48c2588e4e5b9c15a4357f6b0993a7e61aa08971
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-16 16:13:59 +0000

    esopipe-efosc-recipes: add 2.3.9-2 bottle.

--- Formula/esopipe-efosc-recipes.rb
+++ Formula/esopipe-efosc-recipes.rb
@@ -5,6 +5,14 @@ class EsopipeEfoscRecipes < Formula
   sha256 "515a11ddaa6f71d2ebcfff91433b782089f93af5fb7ce3daf973ccfb456f9bba"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-efosc-recipes-2.3.9-2"
+    sha256 arm64_sequoia: "fe292b2d8bd70b545eb5ac45c56c1bd11bc841b3eab8340fe4f7477d14eeead2"
+    sha256 arm64_sonoma:  "bae5aae23ec84dd7d644d27a7f0920d81bd2d2264376c6005fb61bd57f9260ed"
+    sha256 ventura:       "c1cc44b8e761e6810ba502d0678eb3424bf8261c62e20f2ada6fa07f334a83cc"
+    sha256 x86_64_linux:  "489fabc0f14635642dec1582883eee4e7b888d8ef31007d6aff556ff6768ffff"
+  end
+
   def name_version
     "efosc-#{version.major_minor_patch}"
   end
