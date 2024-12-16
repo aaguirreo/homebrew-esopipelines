8ed4ad8c91b8f72b4c31b1bda51102c8891ea7b4
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-16 16:13:59 +0000

    esopipe-efosc: add 2.3.9-2 bottle.

--- Formula/esopipe-efosc.rb
+++ Formula/esopipe-efosc.rb
@@ -5,6 +5,14 @@ class EsopipeEfosc < Formula
   sha256 "515a11ddaa6f71d2ebcfff91433b782089f93af5fb7ce3daf973ccfb456f9bba"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-efosc-2.3.9-2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c09faf650875dcd39980f3b3610ee2ee4799f5748b7df8ccea5924f54152e483"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e3ae81b24b37c462e60e0b8339fb82e97e49db4c1670fb6fc3e5002d38b3273a"
+    sha256 cellar: :any_skip_relocation, ventura:       "c10e78b4a258a3b7094afb5a5573c2a0a5b60ff75c0aeb846cc9f60534cd6c51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a80df7c0b42d326099c2e368e671f335a2ac68fe9fa2c509222bf30c461f46c1"
+  end
+
   def pipeline
     "efosc"
   end
