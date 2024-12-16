be31845b8872738a68c844e73418cdb9aedc2ffc
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-16 17:42:13 +0100

    midi

--- Formula/esopipe-midi-recipes.rb
+++ Formula/esopipe-midi-recipes.rb
@@ -1,28 +1,21 @@
-class EsopipeEfoscRecipes < Formula
-  desc "ESO EFOSC instrument pipeline (recipe plugins)"
+class EsopipeMidiRecipes < Formula
+  desc "ESO MIDI instrument pipeline (recipe plugins)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/efosc/efosc-kit-2.3.9-2.tar.gz"
-  sha256 "515a11ddaa6f71d2ebcfff91433b782089f93af5fb7ce3daf973ccfb456f9bba"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/midi/midi-kit-2.9.6-1.tar.gz"
+  sha256 "f57b483a118fda01335723f51e2eca4c795fa64d3b16dcdcd236b297ea2159d7"
   license "GPL-2.0-or-later"
+
+  def name_version
+    "midi-#{version.major_minor_patch}"
+  end
 
   livecheck do
     url :homepage
-    regex(/href=.*?efosc-kit-(\d+(?:[.-]\d+)+)\.t/i)
-  end
-
-  bottle do
-    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-efosc-recipes-2.3.9-2"
-    sha256 arm64_sequoia: "fe292b2d8bd70b545eb5ac45c56c1bd11bc841b3eab8340fe4f7477d14eeead2"
-    sha256 arm64_sonoma:  "bae5aae23ec84dd7d644d27a7f0920d81bd2d2264376c6005fb61bd57f9260ed"
-    sha256 ventura:       "c1cc44b8e761e6810ba502d0678eb3424bf8261c62e20f2ada6fa07f334a83cc"
-    sha256 x86_64_linux:  "489fabc0f14635642dec1582883eee4e7b888d8ef31007d6aff556ff6768ffff"
-  end
-
-  def name_version
-    "efosc-#{version.major_minor_patch}"
+    regex(/href=.*?midi-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
   depends_on "pkgconf" => :build
+  depends_on "cfitsio@4.2.0"
   depends_on "cpl@7.3.2"
   depends_on "esorex"
 
@@ -32,6 +25,7 @@ class EsopipeEfoscRecipes < Formula
     system "tar", "xf", "#{name_version}.tar.gz"
     cd name_version.to_s do
       system "./configure", "--prefix=#{prefix}",
+                            "--with-cfitsio=#{Formula["cfitsio@4.2.0"].prefix}",
                             "--with-cpl=#{Formula["cpl@7.3.2"].prefix}"
       system "make", "install"
     end
@@ -56,6 +50,6 @@ class EsopipeEfoscRecipes < Formula
   end
 
   test do
-    assert_match "efosc_calib -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page efosc_calib")
+    assert_match "midi_profile -- version #{version.major_minor_patch}", shell_output("#{HOMEBREW_PREFIX}/bin/esorex --man-page midi_profile")
   end
 end
