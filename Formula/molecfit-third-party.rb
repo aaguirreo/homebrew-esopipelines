6e6b4b47d522d5936702881b5efd5c8913b0feab
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-27 10:50:53 +0100

    changed license

--- Formula/molecfit-third-party.rb
+++ Formula/molecfit-third-party.rb
@@ -3,7 +3,7 @@ class MolecfitThirdParty < Formula
   homepage "http://www.eso.org/sci/software/pipelines/skytools/molecfit"
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/molecfit_third_party-1.9.3.tar"
   sha256 "2786e34accf63385932bad66c39deab8c8faf4c9095a23173146a9820f4f0183"
-  license "GPL-2"
+  license "GPL-2.0-or-later"
 
   depends_on "gfortran" => :build
 
