ae83f0b6a9579f1c5f5adb1d84d076e25a5e3a25
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-27 09:21:46 +0000

    esopipe-naco: add 4.4.13-1 bottle.

--- Formula/esopipe-naco.rb
+++ Formula/esopipe-naco.rb
@@ -10,6 +10,14 @@ class EsopipeNaco < Formula
     regex(/href=.*?naco-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-naco-4.4.13-1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f4fb4ee7d55ee2d51858f2955890286eaffc566722b7c2cbb0a358e22b036e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3f2a6442c267611a2fb6e081ec2d995c49f003f7b77a0efb17416aea966f742c"
+    sha256 cellar: :any_skip_relocation, ventura:       "5370b9095f849cea06bbc8d9a7724eb4b87c649dff71b5b4babc3a55de74e7db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c514e58e9e158d1fa64ec3d3527bd6620703b6d0c713fe9ef93686e11edc05cb"
+  end
+
   def pipeline
     "naco"
   end
