9d848f34f678339366cabb6031926de86a102e80
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-27 15:11:44 +0100

    fix for molecfit

--- Formula/molecfit-third-party.rb
+++ Formula/molecfit-third-party.rb
@@ -1,27 +1,29 @@
 class MolecfitThirdParty < Formula
   desc "3rd party tools for the Molecfit library"
-  homepage "https://www.eso.org"
+  homepage "https://www.eso.org/sci/software/pipelines/skytools/molecfit"
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/molecfit_third_party-1.9.3.tar"
   sha256 "2786e34accf63385932bad66c39deab8c8faf4c9095a23173146a9820f4f0183"
   license "GPL-2.0-or-later"
 
-  depends_on "gcc" => :build
-  depends_on "pkgconf" => :build
+  livecheck do
+    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/"
+    regex(/href=.*?molecfit_third_party[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
-  uses_from_macos "curl"
-  uses_from_macos "zlib"
+  depends_on "gcc"
 
   def install
     ENV.deparallelize
+
     system "make", "-f", "BuildThirdParty.mk",
-                   "CC=#{Formula["gcc"].bin}/gcc-14",
-                   "FC=#{Formula["gcc"].bin}/gfortran-14",
-                   "lnfl_target=customOsxGNUsgl",
-                   "lblrtm_target=customOsxGNUsgl",
-                   "prefix=#{prefix}", "install"
+      "CC=#{Formula["gcc"].bin}/gcc-14",
+      "FC=#{Formula["gcc"].bin}/gfortran-14",
+      "prefix=#{prefix}",
+      "install"
   end
 
   test do
+    # Include a basic test to validate installation
     system "true"
   end
 end
