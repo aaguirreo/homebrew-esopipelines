214564961599a3ba8842090ba7ba906127d0f6fd
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-27 11:02:18 +0100

    fixes for molecfit

--- Formula/molecfit-third-party.rb
+++ Formula/molecfit-third-party.rb
@@ -1,10 +1,15 @@
 class MolecfitThirdParty < Formula
   desc "3rd party tools for the Molecfit library"
-  homepage "http://www.eso.org/sci/software/pipelines/skytools/molecfit"
+  homepage "https://www.eso.org/sci/software/pipelines/skytools/molecfit"
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/molecfit_third_party-1.9.3.tar"
   sha256 "2786e34accf63385932bad66c39deab8c8faf4c9095a23173146a9820f4f0183"
   license "GPL-2.0-or-later"
 
+  livecheck do
+    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/molecfit_third_party/"
+    regex(/href=.*?molecfit_third_party[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+  
   depends_on "gfortran" => :build
 
   def install
@@ -15,13 +20,11 @@ class MolecfitThirdParty < Formula
     system "tar", "-xf", cached_download.to_s, "-C", buildpath
     cd "molecfit_third_party-#{version}" do
       system "make", "-f", "BuildThirdParty.mk",
-             "CC=#{Formula["gcc"].bin}/gcc-14",
              "FC=#{Formula["gcc"].bin}/gfortran-14",
              "lnfl_target=#{lnfl_target}",
              "lblrtm_target=#{lblrtm_target}",
              "prefix=#{prefix}"
       system "make", "-f", "BuildThirdParty.mk",
-             "CC=#{Formula["gcc"].bin}/gcc-14",
              "FC=#{Formula["gcc"].bin}/gfortran-14",
              "lnfl_target=#{lnfl_target}",
              "lblrtm_target=#{lblrtm_target}",
