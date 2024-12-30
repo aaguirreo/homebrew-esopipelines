3e8dda058fb7af095129480380831e8b8b2ed8dd
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-30 08:55:25 +0000

    esopipe-sofi: add 1.5.16 bottle.

--- Formula/esopipe-sofi.rb
+++ Formula/esopipe-sofi.rb
@@ -5,6 +5,14 @@ class EsopipeSofi < Formula
   sha256 "ce0fc266650c962291f0ce07fc32b687703fed6c6440a68114022d47c96b165e"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-sofi-1.5.16"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c14f518121b469f8116a9ad0777da7b53c1ec33ed97676e5f9fbc8d11b9c4f94"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "71a82fca22ffe41ebed85c66337d01300045418ae2ba1bf66caf6fde5d5ba3ef"
+    sha256 cellar: :any_skip_relocation, ventura:       "caad4dc2512f843de9cec3e56b8eebe5c9498d1be75ffa31b9c597fbd26e8d30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8960d583bddd14ba42089c58175663173f1584f1f80a724106e9eeb3e5d58eea"
+  end
+
   def pipeline
     "sofi"
   end
