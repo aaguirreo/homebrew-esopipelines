07a7e3a2cce00c9deceb6f2a36e2e9eefc61a078
Alvaro Aguirre <alvaro.aguirre@eso.org>
2024-12-30 09:42:50 +0100

    sofi

--- Formula/esopipe-sofi.rb
+++ Formula/esopipe-sofi.rb
@@ -1,21 +1,24 @@
 class EsopipeSofi < Formula
   desc "ESO SOFI instrument pipeline (static data)"
   homepage "https://www.eso.org/sci/software/pipe_aem_table.html"
-  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/cr2res/cr2re-kit-1.4.4.tar.gz"
-  sha256 "bd4a4d1025ea98ed5372680f877c3b20521109600436c5621e876e994f6f6152"
+  url "https://ftp.eso.org/pub/dfs/pipelines/instruments/sofi/sofi-kit-1.5.16.tar.gz"
+  sha256 "ce0fc266650c962291f0ce07fc32b687703fed6c6440a68114022d47c96b165e"
   license "GPL-2.0-or-later"
+
+  def pipeline
+    "sofi"
+  end
 
   livecheck do
     url :homepage
-    regex(/href=.*?cr2re-kit-(\d+(?:[.-]\d+)+)\.t/i)
+    regex(/href=.*?sofi-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
-  depends_on "esopipe-cr2re-recipes"
+  depends_on "esopipe-sofi-recipes"
 
   def install
-    version_norevision = version.to_s[/(\d+(?:[.]\d+)+)/i, 1]
-    system "tar", "xf", "cr2re-calib-#{version_norevision}.tar.gz"
-    (prefix/"share/esopipes/datastatic/cr2re-#{version_norevision}").install Dir["cr2re-calib-#{version_norevision}/cal/*"]
+    system "tar", "xf", "#{pipeline}-calib-#{version.major_minor_patch}.tar.gz"
+    (prefix/"share/esopipes/datastatic/#{pipeline}-#{version.major_minor_patch}").install Dir["#{pipeline}-calib-#{version.major_minor_patch}/cal/*"]
   end
 
   test do
