1c44874697f64a89859e094fb95f31851ed0d552
Alvaro Aguirre <177130896+aaguirreo@users.noreply.github.com>
2024-12-30 09:58:18 +0000

    esopipe-vimos: add 4.1.10 bottle.

--- Formula/esopipe-vimos.rb
+++ Formula/esopipe-vimos.rb
@@ -10,6 +10,14 @@ class EsopipeVimos < Formula
     regex(/href=.*?vimos-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/aaguirreo/homebrew-esopipelines/releases/download/esopipe-vimos-4.1.10"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04599f8301caa3a772bb4ce9c4e28f5b0c036b6fef63e04553ed611b6eb78470"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eaf1474e7eab226f634227f158bcfda14ea0c9797da67feda0cdf664a263cd26"
+    sha256 cellar: :any_skip_relocation, ventura:       "3eb6fe59432f3bc47ac70a05812a8066d3136fdd4022d81216a73785688a03ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "130be7096d4f84bcc60f17cd1136b9228d6b14a64ef49324255baf35d37fa6c5"
+  end
+
   def pipeline
     "vimos"
   end
